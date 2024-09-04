5e3034e342b14a64a2837f8b45c7a56b3a66f326
eunhye <128738657+eunhyes@users.noreply.github.com>
2024-09-04 12:40:50 +0900

    [Bronze V] Title: 구구단, Time: 208 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int i;
+		
+		for(i=1; i<=9; i++) {
+			
+			System.out.println(n + " * " + i + " = " + n*i);
+		}
+		
+	}
+	
+}
