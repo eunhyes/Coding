931d6a549aab4ac52a825f9137066dd1124da695
eunhye <128738657+eunhyes@users.noreply.github.com>
2024-09-26 23:23:10 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		int k = sc.nextInt();
+		int h = sc.nextInt();
+		
+		if(s + k + h >= 100) {
+			System.out.println("OK");
+		} else if(s < k && s < h) {
+			System.out.println("Soongsil");
+		} else if(k < s && k < h) {
+			System.out.println("Korea");
+		} else {
+			System.out.println("Hanyang");
+		}
+		
+	}
+
+}
