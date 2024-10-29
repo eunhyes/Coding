27d29dd0c1e4fe0c8dfc862a29f27ab81d194ae8
eunhye <128738657+eunhyes@users.noreply.github.com>
2024-10-29 12:36:26 +0900

    [Bronze III] Title: 나는 요리사다, Time: 196 ms, Memory: 18484 KB -BaekjoonHub

---
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int max = 0;
+		int max_num = 0;
+		
+		for(int i=1; i<=5; i++) {
+			int sum = 0;
+			
+			for(int j=0; j<4; j++) {
+				int score = sc.nextInt();
+				sum += score;
+			}
+			
+			if(max < sum) {
+				max = sum;
+				max_num = i;
+			}
+			
+		}
+		System.out.println(max_num + " " + max);
+	}
+
+}
