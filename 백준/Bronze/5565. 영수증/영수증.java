e883d5fef2de2fba4d54ea3d5bdf9077c8615057
eunhye <128738657+eunhyes@users.noreply.github.com>
2024-10-31 23:22:08 +0900

    [Bronze III] Title: 영수증, Time: 176 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int total = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=0; i<9; i++) {
+			int price = sc.nextInt();
+			sum += price;
+		}
+		
+		int answer = total-sum;
+		System.out.println(answer);
+	}
+}
